c66c7567686252d94ff2d73c17648e4f5619c114
Aliye Tefikova <tefikova1996@mail.ru>
2016-03-03 04:02:00 +0300

    creation (mode2) first form added

---
+++ Socionic_tests/Socionic_tests/Skins/creation_form1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Socionic_tests.Skins
+{
+    /// <summary>
+    /// Interaction logic for creation_form1.xaml
+    /// </summary>
+    public partial class creation_form1 : Window
+    {
+        public creation_form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
