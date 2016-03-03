5fb61eb2d42b341158213b30179a2593734d444d
Aliye Tefikova <tefikova1996@mail.ru>
2016-03-03 22:28:56 +0300

    testing mode first form added

---
+++ Socionic_tests/Socionic_tests/Skins/testing_form1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Socionic_tests.Skins
+{
+    /// <summary>
+    /// Interaction logic for testing_form1.xaml
+    /// </summary>
+    public partial class testing_form1 : Window
+    {
+        public testing_form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
