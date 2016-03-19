d5cbf0ac5efa8b1aaca32df8223eb31a7e8e13e4
VasilevIgor <iavasilev_2@edu.hse.ru>
2016-03-19 16:04:43 +0300

    CreationVievModel_2 is created

---
+++ Socionic_tests/Socionic_tests/ViewModel/CreationVievModel_2.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace Socionic_tests.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class CreationVievModel_2 : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the CreationVievModel_2 class.
+        /// </summary>
+        public CreationVievModel_2()
+        {
+        }
+    }
+}
