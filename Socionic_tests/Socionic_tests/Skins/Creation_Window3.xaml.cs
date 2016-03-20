1f9681c410e246d410094b8b54fe01e22b87edd4
Aliye Tefikova <tefikova1996@mail.ru>
2016-03-20 19:54:20 +0300

    some edtitions including text, button style etc

--- Socionic_tests/Socionic_tests/Skins/Creation_Window3.xaml.cs
+++ Socionic_tests/Socionic_tests/Skins/Creation_Window3.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace Socionic_tests.Skins
 {
@@ -22,6 +23,29 @@ namespace Socionic_tests.Skins
         public Creation_Window3()
         {
             InitializeComponent();
+            //подписываем textBox на событие PreviewTextInput, 
+            //с помощью которого можно обрабатывать вводимый текст
+            textBox1.PreviewTextInput += new TextCompositionEventHandler(textBox_PreviewTextInput);
         }
+
+        //создаем регулярное выражение, описывающее правило ввода
+        //в данном случае, это символы от 0 до 9
+        Regex inputRegex = new Regex(@"^[0-9]$");
+
+        //сам обработчик события PreviewTextInput для элемента textBox
+        void textBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            //проверяем или подходит введенный символ нашему правилу
+            Match match = inputRegex.Match(e.Text);
+            //и проверяем или выполняется условие
+            //если количество символов в строке больше или равно трем либо
+            //если введенный символ не подходит нашему правилу
+            if ((sender as TextBox).Text.Length >= 3 || !match.Success)
+            {
+                //то обработка события прекращается и ввода неправильного символа не происходит
+                e.Handled = true;
+            }
+        }
+
     }
 }
